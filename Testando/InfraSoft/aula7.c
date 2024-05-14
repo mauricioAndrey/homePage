3eeeabbd005fa3c3745249e1932f12eef503909e
mayres-2 <mfss2@cin.ufpe.br>
2024-05-14 07:08:27 -0300

    Update Multiplicaçao de matrizes

--- Testando/InfraSoft/aula7.c
+++ Testando/InfraSoft/aula7.c
@@ -144,7 +144,7 @@ int resultado[3][3];
 
 void *threadCode(void *tid){   
 	int i,j,k;
-  int  threadId = (*(int *)tid); 
+  int threadId = (*(int *)tid); 
   for(i=threadId; i < LINHAS; i = i + NUM_THREADS) {       
     for (j=0;j<COLUNAS;j++) { 
       resultado[i][j] = 0; 
@@ -158,11 +158,12 @@ void *threadCode(void *tid){
 int main (int argc, char *argv[]){   
 	pthread_t threads[NUM_THREADS]; 
   int *taskids[NUM_THREADS];
-	int i,j,u; int t;   
+	int i,j,u,t; 
 	
   for(t=0; t<NUM_THREADS; t++){
     printf("No main: criando thread %d\n", t);    
-    taskids[t] = (int *) malloc(sizeof(int)); *taskids[t] = t;
+    taskids[t] = (int *) malloc(sizeof(int)); 
+    *taskids[t] = t;
 	 	pthread_create(&threads[t],NULL,threadCode, (void *)taskids[t]); 
   }
   for(u=0; u<NUM_THREADS;u++) {
--- Testando/teste.c
+++ Testando/teste.c
@@ -5,43 +5,89 @@
 #include <string.h>
 //#include <unistd.h>
 
-#define NUM_THREADS 2
-#define LINHAS 3
-#define COLUNAS 3
 
-int matriz1[LINHAS][COLUNAS] = {{1,2,5}, {3,4,2}, {5,6,1}};
-int matriz2[LINHAS][COLUNAS] = {{1,2,4}, {3,4,2}, {5,6,1}};
-int resultado[LINHAS][COLUNAS];
+struct matriz{
+    int **mat1;
+    int **mat2;
+    int **res;
+    int LINHAS,COLUNAS;
+}; 
+struct threadThings{
+    struct matriz *mat;
+    pthread_t thread;
+    int *NUM_THREADS;
+    int *id;
+    int linha, coluna;
+};
 
-void *linhaThread(void *arg){
-    int i,j,k;
-    int *id = (int*) arg;
-    for(i = (*id); i<LINHAS; i=i+NUM_THREADS) {
-        for(j=0;j<COLUNAS; j++){
-            resultado[i][j] = 0;
-            for(k=0; k<COLUNAS; k++)
-                resultado[i][j] = resultado[i][j] + matriz1[i][k]*matriz2[k][j];
-        }
+
+void *celulaThread(void *arg){
+    struct threadThings* info = (struct threadThings*) arg;
+    int i=info->linha;
+    int j=info->coluna;
+    int k;
+    for(k=0; k<info->mat->COLUNAS; k++){
+        info->mat->res[i][j] = info->mat->res[i][j] + 
+            info->mat->mat1[i][k]*info->mat->mat2[k][j];
     }
 }
 
 int main(int argc, char *argv[]){
-    pthread_t threads[NUM_THREADS];
-    int *ids[NUM_THREADS];
-    int i,j;
-    for(i=0;i<NUM_THREADS; i++){
-        printf("Na main: criando thread %d\n", i);
-        ids[i]=(int*)malloc(sizeof(int));
-        *ids[i] = i;
-        pthread_create(&threads[i], NULL, linhaThread, (void *) ids[i]);
+    struct matriz mat;
+    
+    FILE *arq = fopen("arqTeste.txt", "r");
+    if(arq == NULL) { return 1; }
+    char buffer;
+
+    fscanf(arq, "%d %d", &mat.LINHAS, &mat.COLUNAS);
+    fgets(&buffer, sizeof(char), arq);
+    ///
+    mat.mat1 = (int **) malloc (sizeof(int*));
+    mat.res = (int **) malloc(sizeof(int*));
+    for(int i=0; i<mat.LINHAS; i++){
+        mat.mat1[i] = (int *) malloc(sizeof(int));
+        mat.res[i] = (int*) malloc(sizeof(int));
+        for(int j=0; j<mat.COLUNAS; j++){
+            fscanf(arq, "%d", &mat.mat1[i][j]);
+            fgets(&buffer, sizeof(char), arq);
+        }
     }
-    for(j=0; j<LINHAS; j++){
+    //
+    mat.mat2 = (int **) malloc (sizeof(int*));
+    for(int i=0; i<mat.LINHAS; i++){
+        mat.mat2[i] = (int *) malloc(sizeof(int));
+        for(int j=0; j<mat.COLUNAS; j++){
+            fscanf(arq, "%d", &mat.mat2[i][j]);
+            fgets(&buffer, sizeof(char), arq);
+        }
+    }
+    ///
+    int NUM_THREADS = mat.LINHAS * mat.COLUNAS;
+    struct threadThings **threads = (struct threadThings **) 
+        malloc(sizeof(struct threadThings*)*NUM_THREADS);
+    
+    int t=0,i=0,j=0;
+    for(t=0;t<NUM_THREADS; t++){
+        threads[t] = (struct threadThings*) malloc(sizeof(struct threadThings));
+        (*threads)[t].mat = &mat;
+        (*threads)[t].id = &t;
+        (*threads)[t].NUM_THREADS = &NUM_THREADS;
+        (*threads)[t].linha = i;
+        if(i==mat.LINHAS) i=0;
+        else if(i<mat.LINHAS) i++;
+        (*threads)[t].coluna = j;
+        if(j==mat.COLUNAS) j=0;
+        else if(j<mat.COLUNAS && t%mat.COLUNAS == 0) j++;
+        printf("Na main: criando thread %d\n", t);
+        pthread_create(&threads[t]->thread, NULL, celulaThread, (void *) threads[t]);
+    }
+    for(t=0;t<NUM_THREADS; t++){
         //int *res;
-        pthread_join(threads[i], NULL);
+        pthread_join((*threads)[t].thread, NULL);
     }
-    for(i=0; i<LINHAS; i++){
-        for(j=0; j<COLUNAS; j++)
-            printf("%d ", resultado[i][j]);
+    for(i=0; i<mat.LINHAS; i++){
+        for(j=0; j<mat.COLUNAS; j++) 
+            printf("%d ", mat.res[i][j]);
         printf("\n");
     }
     pthread_exit(NULL);
